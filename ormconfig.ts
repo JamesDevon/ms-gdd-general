a8d79ac69301b9b66df6d7c52b61315d11faac71
JamesDevon <jbalashi@gmail.com>
2022-05-07 21:15:13 +0300

    dotenv addition

---
+++ ormconfig.ts
@@ -0,0 +1,23 @@
+import {TypeOrmModuleOptions} from "@nestjs/typeorm/dist/interfaces/typeorm-options.interface";
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export class DbConfig {
+
+    private static readonly config: TypeOrmModuleOptions = {
+        type: 'mysql',
+        host: process.env.MYSQL_ADDRESS,
+        port: Number(process.env.MYSQL_PORT),
+        username: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        database: process.env.MYSQL_DB,
+        autoLoadEntities: true,
+        synchronize: true,
+    };
+
+
+    public static getConfig() {
+        return this.config;
+    }
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import {TypeOrmModule} from "@nestjs/typeorm";
+import {DbConfig} from '../ormconfig';
 
 @Module({
-  imports: [ TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: 'root',
-    database: 'gdd',
-    autoLoadEntities: true,
-    synchronize: true,
-  }),
+  imports: [ TypeOrmModule.forRoot(DbConfig.getConfig()),
     AuthModule],
 })
 export class AppModule {}
