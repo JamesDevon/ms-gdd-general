bba30b0db6b8f154f667de73ea963ea0c8849279
JamesDevon <jbalashi@gmail.com>
2022-07-08 19:59:55 +0300

    fixed duplication of text saving

--- src/websockets/editor/editor.gateway.ts
+++ src/websockets/editor/editor.gateway.ts
@@ -18,28 +18,33 @@ export class EditorGateway implements NestGateway   {
 
     afterInit(server: Server): void {
         this.server = server;
-        this.logger.log('WebSocket init!');
+        this.logger.debug('WebSocket init!');
     }
 
     handleConnection(client: Socket, ...args: any[]): void {
-        this.logger.log(`client connected: ${client.id}`);
+        this.logger.debug(`client connected: ${client.id}`);
     }
 
     handleDisconnect(client: any): void {
-        this.logger.log(`client disconnected: ${client.id}`);
+        this.logger.debug(`client disconnected: ${client.id}`);
     }
 
     @SubscribeMessage('get-document')
     async getDocument(client: Socket, projectId: string): Promise<void> {
+        this.logger.debug("User subscribed to get-document with id : "+projectId);
         if (projectId == null) return;
         const project: Project = await this.projectService.getProjectById(projectId);
         client.join(projectId);
+        this.logger.debug("User joined room : "+projectId);
         client.emit("load-document",  project.data || {});
+        this.logger.debug("Data emitted : "+projectId);
         client.on("save-document", async (dataToSave:String) => {
+            this.logger.debug("Saving document : "+projectId);
             project.data = dataToSave;
             await this.projectService.updateProject(project);
         })
         client.on('send-changes', (delta: string) => {
+            this.logger.debug("Broadcasting changes : "+projectId);
             client.broadcast.to(projectId).emit('receive-changes',delta);
         });
     }
