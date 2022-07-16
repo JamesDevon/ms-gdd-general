f6b08d830e54f2135036b4daf9e14247f8d657d1
JamesDevon <jbalashi@gmail.com>
2022-07-16 18:55:37 +0300

    update paths to absolue

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from 'src/api/auth/auth.module';
 import {TypeOrmModule} from "@nestjs/typeorm";
-import {Mysql} from 'config/Mysql';
+import {Mysql} from 'src/config/Mysql';
 import {ProjectModule} from "src/api/project/project.module";
 import {MongooseModule} from "@nestjs/mongoose";
 import { ProfileModule } from 'src/api/profile/profile.module';
 import {EditorModule} from "src/websockets/editor/editor.module";
-import {MongoDb} from "config/MongoDb";
+import {MongoDb} from "src/config/MongoDb";
 
 
 @Module({
