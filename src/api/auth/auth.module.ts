3c0973bad85977937e647281e9de723929648b0c
JamesDevon <jbalashi@gmail.com>
2022-07-16 18:26:28 +0300

    try absolute path

--- src/api/auth/auth.module.ts
+++ src/api/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import {TypeOrmModule} from "@nestjs/typeorm";
-import {UsersRepository} from "./entities/users/users.repository";
+import {UsersRepository} from "src/api/auth/entities/users/users.repository";
 import { PassportModule } from '@nestjs/passport';
 import {JwtModule} from "@nestjs/jwt";
 import { JwtStrategy } from './utils/jwt.strategy';
