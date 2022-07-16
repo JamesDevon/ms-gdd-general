cd5f7dec73ae45ae3edd9a3ed7cc5b997c79af36
JamesDevon <jbalashi@gmail.com>
2022-07-16 17:41:57 +0300

    test

--- config/Mysql.ts
+++ config/Mysql.ts
@@ -1,6 +1,5 @@
 import {TypeOrmModuleOptions} from "@nestjs/typeorm/dist/interfaces/typeorm-options.interface";
 import * as dotenv from 'dotenv';
-import {User} from "../src/api/auth/entities/users/user.entity";
 
 dotenv.config();
 
@@ -13,8 +12,8 @@ export class Mysql {
         username: process.env.MYSQL_USER,
         password: process.env.MYSQL_PASSWORD,
         database: process.env.MYSQL_DB,
+        autoLoadEntities: true,
         synchronize: true,
-        entities: [User],
     };
 
 
